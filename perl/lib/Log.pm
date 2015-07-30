9c19f1fa10b07e1c13782bd8cc304f7923e6242b
karur4n <karuran.f@gmail.com>
2015-07-30 21:41:40 +0900

    課題 Perl-1 を追加

--- perl/lib/Log.pm
+++ perl/lib/Log.pm
@@ -2,24 +2,41 @@ package Log;
 use strict;
 use warnings;
 
+use DateTime;
+
 sub new {
     my ($class, %args) = @_;
     return bless \%args, $class;
 }
 
 sub protocol {
+  my $self = shift;
+  my @request = split(' ', $self->{req});
+  return $request[2];
 }
 
 sub method {
+  my $self = shift;
+  my @request = split(' ', $self->{req});
+  return $request[0];
 }
 
 sub path {
+  my $self = shift;
+  my @request = split(' ', $self->{req});
+  return $request[1];
 }
 
 sub uri {
+  my $self = shift;
+  my $host = $self->{host};
+  return 'http://' . $host . $self->path;
 }
 
 sub time {
+  my $self = shift;
+  my $dt = DateTime->from_epoch(epoch => $self->{epoch});
+  return $dt;
 }
 
 1;
