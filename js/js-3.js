e6cf7906d1940e9b968649c1196327041c4225b5
karur4n <karuran.f@gmail.com>
2015-07-31 15:26:07 +0900

    課題 JS-3 の追加

--- js/js-3.js
+++ js/js-3.js
@@ -1,1 +1,11 @@
 // 課題 JS-3 の実装をここに記述してください。
+
+var submit_button = document.getElementById('submit-button');
+var container = document.getElementById('table-container');
+
+submit_button.addEventListener('click', function() {
+  var input_log = document.getElementById('log-input').value;
+  var parsed_log = parseLTSVLog(input_log);
+
+  createLogTable(container, parsed_log);
+});
