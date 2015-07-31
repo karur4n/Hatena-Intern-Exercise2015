2f026a15c0090f6dd6d9b58e45a31e09cf440118
karur4n <karuran.f@gmail.com>
2015-07-31 14:49:02 +0900

    課題 Perl-3 の追加

--- perl/lib/LogCounter.pm
+++ perl/lib/LogCounter.pm
@@ -2,15 +2,37 @@ package LogCounter;
 use strict;
 use warnings;
 
+use Data::Dumper;
+
 sub new {
     my ($class, $logs) = @_;
     return bless { logs => $logs }, $class;
 };
 
 sub group_by_user {
+  my $self = shift;
+  my %return;
+  for my $log (@{$self->{logs}}) {
+    if ($log->{user}) {
+      push @{$return{$log->{user}}}, $log;
+    } else {
+      push @{$return{'guest'}}, $log;
+    }
+  }
+
+  \%return;
 }
 
 sub count_error {
+  my $self = shift;
+  my $count = 0;
+  for my $log (@{$self->{logs}}) {
+    if ($log->{status} =~ /^[5]\d{2}$/) {
+      $count++;
+    }
+  };
+
+  return $count;
 }
 
 1;
