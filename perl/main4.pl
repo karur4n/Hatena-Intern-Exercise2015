1c080d682f3b78e666c3c0c1efd2318e4ac1667d
karur4n <karuran.f@gmail.com>
2015-08-01 21:00:27 +0900

    課題 Perl-4 の追加

---
+++ perl/main4.pl
@@ -0,0 +1,43 @@
+use strict;
+use warnings;
+
+use DateTime;
+use Data::Dumper;
+
+use Parser;
+
+my $parser = Parser->new(filename => '../sample_data/large_log.ltsv');
+my $parsed_logs = $parser->parse;
+my $i;
+
+my %result;
+
+print "毎分のリクエスト URI の分布\n";
+
+for my $log (@{$parsed_logs}) {
+  my $formatted_time = substr($log->time, 0, 16);
+  $result{$formatted_time}->{$log->uri}++;
+}
+
+for my $key (sort keys %result) {
+  print "\n$key  ";
+
+  for ($i = 1; $i <= 40; $i++) {
+    if ($i == 1 || $i % 5 == 0) {
+      print $i;
+    } else {
+      print " ";
+    }
+  }
+
+  print "\n";
+
+  for (sort keys %{$result{$key}}) {
+    my $path = substr($_, 16);
+    printf("%16s: ", $path);
+    for ($i = 0; $i < $result{$key}->{$_}; $i++) {
+      print "=";
+    }
+    print "\n";
+  }
+}
