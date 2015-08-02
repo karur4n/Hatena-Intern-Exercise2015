f3e11a8ce53b2ca322b189f887e28981a60e2dda
karur4n <karuran.f@gmail.com>
2015-08-02 14:26:55 +0900

    変数名を複数形に修正する

--- js/js-3.js
+++ js/js-3.js
@@ -4,8 +4,8 @@ var submit_button = document.getElementById('submit-button');
 var container = document.getElementById('table-container');
 
 submit_button.addEventListener('click', function() {
-  var input_log = document.getElementById('log-input').value;
-  var parsed_log = parseLTSVLog(input_log);
+  var input_logs = document.getElementById('log-input').value;
+  var parsed_logs = parseLTSVLog(input_logs);
 
-  createLogTable(container, parsed_log);
+  createLogTable(container, parsed_logs);
 });
